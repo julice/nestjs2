e88f9d7a8eb56aa9b9064b7f7db9747c37c4e1e0
fangliji <736419409@qq.com>
2023-03-07 21:37:38 +0800

    拦截成功nest g interceptor core/interceptor/transform

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './core/filter/http-exception/http-exception.filter';
+import { TransformInterceptor } from './core/interceptor/transform/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 注册全局错误的过滤器
   app.useGlobalFilters(new HttpExceptionFilter());
+  // 注册全局拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
 bootstrap();
