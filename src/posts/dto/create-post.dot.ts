7a25da1766b0898b185db23e70123ff2db921860
fangliji <736419409@qq.com>
2023-03-07 22:26:17 +0800

    加class-validator数据验证

--- src/posts/dto/create-post.dot.ts
+++ src/posts/dto/create-post.dot.ts
@@ -1,14 +1,18 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreatePostDto {
   @ApiProperty({ description: '文章标题' })
+  @IsNotEmpty({ message: '文章标题必填' })
   readonly title: string;
 
   @ApiProperty({ description: '作者' })
+  @IsNotEmpty({ message: '缺少作者信息' })
   readonly author: string;
 
   // 必选
   @ApiProperty({ description: '内容' })
+  @IsNotEmpty({ message: '缺少文章内容' })
   readonly content: string;
   // 可选
   @ApiPropertyOptional({ description: '封面' })
